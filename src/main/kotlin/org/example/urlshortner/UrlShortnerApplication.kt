2743ea130a0b0d26dc5ea86f5603aba1fa510b81
affan.sheikh <affan.sheikh@olx.com>
2024-07-12 18:43:05 +0200

    add cache manager to run the application

--- src/main/kotlin/org/example/urlshortner/UrlShortnerApplication.kt
+++ src/main/kotlin/org/example/urlshortner/UrlShortnerApplication.kt
@@ -1,13 +1,18 @@
 package org.example.urlshortner
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.example.urlshortner.db.repositories.UrlRepository
 import org.example.urlshortner.db.repositories.UrlRepositoryImpl
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.CachingConfigurerSupport
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
 
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class, HibernateJpaAutoConfiguration::class] )
@@ -23,4 +28,19 @@ class BeanDeclaration {
 
     @Bean
     fun urlRepository(): UrlRepository = UrlRepositoryImpl()
+}
+
+@Configuration
+class CacheConfiguration : CachingConfigurerSupport() {
+
+    @Bean
+    fun urlCacheManager(): CacheManager {
+        val cacheManager = CaffeineCacheManager("urls")
+        cacheManager.setCaffeine(
+            Caffeine.newBuilder()
+                .initialCapacity(10)
+                .maximumSize(5L)
+        )
+        return cacheManager
+    }
 }
