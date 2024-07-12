8cdf0175698dd99c363a67af41a381a9a5aa1b65
affan.sheikh <affan.sheikh@olx.com>
2024-07-12 18:09:13 +0200

    add openapi documentation

--- src/main/kotlin/org/example/urlshortner/UrlShortnerApplication.kt
+++ src/main/kotlin/org/example/urlshortner/UrlShortnerApplication.kt
@@ -1,10 +1,14 @@
 package org.example.urlshortner
 
+import org.example.urlshortner.db.repositories.UrlRepository
+import org.example.urlshortner.db.repositories.UrlRepositoryImpl
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
 
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class, HibernateJpaAutoConfiguration::class] )
 @EnableCaching
@@ -12,4 +16,11 @@ class UrlShortnerApplication
 
 fun main(args: Array<String>) {
     runApplication<UrlShortnerApplication>(*args)
+}
+
+@Component
+class BeanDeclaration {
+
+    @Bean
+    fun urlRepository(): UrlRepository = UrlRepositoryImpl()
 }
